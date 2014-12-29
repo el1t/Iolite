60ebb70f235295014bef387b2158d8db5b96876b
tcyrus <tim.h.cyrus@gmail.com>
2014-12-29 10:00:51 -0500

    Fixed Null Error During Debug
    At mLoginFragment.setUsername(username) the code would throw a null
    value exception because mLoginFragment wasn’t defined yet

--- app/src/main/java/com/el1t/iolite/LoginActivity.java
+++ app/src/main/java/com/el1t/iolite/LoginActivity.java
@@ -66,8 +66,8 @@ public class LoginActivity extends ActionBarActivity implements LoginFragment.On
 		final Cookie[] cookies = getCookies(preferences);
 		final boolean remember = preferences.getBoolean("remember", false);
 		final String username = preferences.getString("username", null);
+        mLoginFragment = new LoginFragment();
 		if (savedInstanceState == null) {
-			mLoginFragment = new LoginFragment();
 			// Restore saved username
 			final Bundle args = new Bundle();
 			args.putBoolean("remember", remember);
