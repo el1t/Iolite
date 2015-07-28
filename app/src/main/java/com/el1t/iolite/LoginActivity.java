07f6f17dc7813824345b8d3e96e37d4bad5c7bef
Ellis Tsung <el1t@ymail.com>
2015-07-29 00:24:39 +0200

    Remove cookie integration from login

--- app/src/main/java/com/el1t/iolite/LoginActivity.java
+++ app/src/main/java/com/el1t/iolite/LoginActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
@@ -21,7 +20,6 @@ import com.el1t.iolite.parser.ProfileJsonParser;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.HttpCookie;
 import java.net.URL;
 import java.util.List;
 
@@ -34,13 +32,11 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 	public static final String PREFS_NAME = "LOGIN";
 	private static final String TAG = "Login Activity";
 
-	static java.net.CookieManager mCookieManager = new java.net.CookieManager();
 	private LoginFragment mLoginFragment;
 	private ProgressDialog mProgressDialog;
 	private String login_username;
 	private String login_password;
 	private String mAuthKey;
-	private int attempt;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +60,6 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 			mLoginFragment = (LoginFragment) getFragmentManager().getFragment(savedInstanceState, "loginFragment");
 		}
 
-		attempt = 0;
 		// This is identical to checkAuthentication except for intent checking
 		if ((mAuthKey = Utils.getAuthKey(preferences)) != null) {
 			if (getIntent().getBooleanExtra("logout", false)) {
@@ -72,7 +67,7 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 				logout();
 			} else {
 				// Check authentication
-				new Authentication().execute("https://ion.tjhsst.edu/api/profile?format=json");
+				new Authentication().execute();
 			}
 		} else {
 			// Show keyboard
@@ -92,25 +87,8 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 		getFragmentManager().putFragment(savedInstanceState, "loginFragment", mLoginFragment);
 	}
 
-	private static void storeCookies(List<String> cookies) {
-		for (String cookie : cookies) {
-			mCookieManager.getCookieStore().add(null, HttpCookie.parse(cookie).get(0));
-		}
-	}
-
-	public static String getCookies() {
-		if(mCookieManager.getCookieStore().getCookies().size() > 0) {
-			return TextUtils.join(",", mCookieManager.getCookieStore().getCookies());
-		}
-		return "";
-	}
-
-	private void clearCookies() {
-		mCookieManager.getCookieStore().removeAll();
-	}
-
 	private void checkAuthentication() {
-		new Authentication().execute("https://ion.tjhsst.edu/api/profile?format=json");
+		new Authentication().execute();
 	}
 
 	// Submit the login request
@@ -138,41 +116,30 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 		} else if (login_password.isEmpty()) {
 			mLoginFragment.showError(LoginFragment.ErrorType.EMPTY_PASSWORD);
 		} else {
-			new LoginRequest().execute("https://ion.tjhsst.edu/api");
+			new LoginRequest().execute();
 		}
 	}
 
 	// Do after authentication request
 	void postRequest(User user, boolean fake) {
-		if (user != null) {
-			if (fake) {
-				Log.d(TAG, "Loading test data");
-			} else {
-				final SharedPreferences.Editor preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
-				if (login_username != null) {
-					preferences.putString("username", login_username);
-				}
-				if (login_password != null) {
-					preferences.putString("password", login_password);
-				}
-				preferences.putBoolean("remember", mLoginFragment.isChecked());
-				preferences.apply();
-			}
-			attempt = 0;
-			final Intent intent = new Intent(this, HomeActivity.class);
-			intent.putExtra("fake", fake);
-			intent.putExtra("user", user);
-			startActivity(intent);
-			finish();
+		if (fake) {
+			Log.d(TAG, "Loading test data");
 		} else {
-			Snackbar.make(findViewById(R.id.container), "Session Expired", Snackbar.LENGTH_SHORT).show();
-			Log.d(TAG, "Session Expired");
-			clearCookies();
-			final SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-			if (preferences.getBoolean("remember", false) && attempt < 1) {
-				submit(preferences.getString("username", null), preferences.getString("password", null));
+			final SharedPreferences.Editor preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+			if (login_username != null) {
+				preferences.putString("username", login_username);
 			}
+			if (login_password != null) {
+				preferences.putString("password", login_password);
+			}
+			preferences.putBoolean("remember", mLoginFragment.isChecked());
+			preferences.apply();
 		}
+		final Intent intent = new Intent(this, HomeActivity.class);
+		intent.putExtra("fake", fake);
+		intent.putExtra("user", user);
+		startActivity(intent);
+		finish();
 	}
 
 	private void logout() {
@@ -195,13 +162,13 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 	}
 
 	// Login request using HttpPost
-	private class LoginRequest extends AsyncTask<String, Void, List<String>> {
+	private class LoginRequest extends AsyncTask<Void, Void, Boolean> {
 		private static final String TAG = "Login Connection";
+		private static final String URL = "https://ion.tjhsst.edu/api";
 		private HttpsURLConnection mConnection;
 
 		@Override
 		protected void onPreExecute() {
-			attempt++;
 			if (mProgressDialog == null || !mProgressDialog.isShowing()) {
 				mProgressDialog = new ProgressDialog(LoginActivity.this);
 				mProgressDialog.setMessage("Logging In");
@@ -225,14 +192,14 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 		}
 
 		@Override
-		protected List<String> doInBackground(String... urls) {
+		protected Boolean doInBackground(Void... params) {
 			try {
-				mConnection = (HttpsURLConnection) new URL(urls[0]).openConnection();
+				mConnection = (HttpsURLConnection) new URL(URL).openConnection();
 				mAuthKey = "Basic " + Base64.encodeToString((login_username + ":" + login_password).getBytes(), Base64.NO_WRAP);
 				mConnection.setRequestProperty("Authorization", mAuthKey);
 				mConnection.setUseCaches(false);
 				mConnection.connect();
-				return mConnection.getHeaderFields().get("Set-Cookie");
+				return true;
 			} catch (FileNotFoundException e) {
 				try {
 					if (mConnection.getResponseCode() == 401) {
@@ -266,19 +233,19 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 							}).show();
 				}
 			} catch (Exception e) {
+				Snackbar.make(findViewById(R.id.container), "Connection error", Snackbar.LENGTH_SHORT).show();
 				Log.e(TAG, "Login error", e);
 			} finally {
 				mConnection.disconnect();
 			}
-			return null;
+			return false;
 		}
 
 		@Override
-		protected void onPostExecute(List<String> result) {
+		protected void onPostExecute(Boolean result) {
 			super.onPostExecute(result);
 			mProgressDialog.dismiss();
-			if (result != null) {
-				storeCookies(result);
+			if (result) {
 				checkAuthentication();
 			}
 		}
@@ -289,9 +256,10 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 		}
 	}
 
-	// Test if cookies are functional
-	private class Authentication extends AsyncTask<String, Void, User> {
+	// Load student profile data
+	private class Authentication extends AsyncTask<Void, Void, User> {
 		private static final String TAG = "Authentication";
+		private static final String URL = "https://ion.tjhsst.edu/api/profile?format=json";
 		private HttpsURLConnection mConnection;
 
 		@Override
@@ -319,30 +287,47 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 		}
 
 		@Override
-		protected User doInBackground(String... urls) {
-			User response;
+		protected User doInBackground(Void... params) {
+			final User response;
 			try {
-				mConnection = (HttpsURLConnection) new URL(urls[0]).openConnection();
+				mConnection = (HttpsURLConnection) new URL(URL).openConnection();
 				// Attach authentication
 				mConnection.setRequestProperty("Authorization", mAuthKey);
 				mConnection.setUseCaches(false);
 				// Begin connection
 				mConnection.connect();
-				// Parse xml from server
+				// Parse JSON from server
 				response = ProfileJsonParser.parse(mConnection.getInputStream());
 				// Close connection
 				mConnection.disconnect();
 				return response;
 			} catch(IOException e) {
-				Log.e(TAG, "Parse error.", e);
+				// Hide soft keyboard
+				final InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+				final View view = getCurrentFocus();
+				if (view != null && imm.isAcceptingText()) {
+					imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+				}
+				if (isCancelled()) {
+					Snackbar.make(findViewById(R.id.container), "Cancelled", Snackbar.LENGTH_SHORT)
+							.setAction("Retry", new View.OnClickListener() {
+								@Override
+								public void onClick(View v) {
+									checkAuthentication();
+								}
+							}).show();
+				} else {
+					Snackbar.make(findViewById(R.id.container), "Cannot connect to server", Snackbar.LENGTH_SHORT)
+							.setAction("Retry", new View.OnClickListener() {
+								@Override
+								public void onClick(View v) {
+									checkAuthentication();
+								}
+							}).show();
+				}
 			} catch (Exception e) {
 				Snackbar.make(findViewById(R.id.container), "Connection error", Snackbar.LENGTH_SHORT).show();
 				Log.e(TAG, "Connection error.", e);
-			}
-			try {
-				Log.d(TAG, "Response: " + mConnection.getResponseCode());
-			} catch (Exception e) {
-
 			}
 			return null;
 		}
@@ -351,8 +336,9 @@ public class LoginActivity extends AppCompatActivity implements LoginFragment.On
 		protected void onPostExecute(User result) {
 			super.onPostExecute(result);
 			mProgressDialog.dismiss();
-
-			postRequest(result, false);
+			if (result != null) {
+				postRequest(result, false);
+			}
 		}
 	}
 }
